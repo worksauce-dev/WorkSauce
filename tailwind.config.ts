419810b278b076781a33887abbbd1b18ee723c7e
ethanJcoding <ethan.95j@gmail.com>
2024-08-14 19:03:04 +0900

    🔧 tailwind config 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,43 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        light: "#f8fafb",
+        dark: "#1C1C1E",
+
+        "primary-gray": "#636366",
+        placeholder: "AEAEB2",
+        border: "#C6C6C6",
+
+        "primary-blue": "#004DDE",
+        "secondary-blue": "#DBEAFE",
+
+        "primary-green": "#14AE5C",
+        "secondary-green": "#CFF7D3",
+
+        "primary-yellow": "#EEC800",
+        "secondary-yellow": "#FFF1C2",
+
+        "primary-accent": "#0F62FE",
+        "secondary-accent": "#ffd2d6",
+      },
+      fontSize: {
+        caption: "12px",
+        body1: "14px",
+        body2: "16px",
+        subheading: "18px",
+        heading1: "22px",
+        heading2: "28px",
+        heading3: "38px",
+        hero: "50px",
+      },
+
+      fontWeight: {
+        light: "300",
+        medium: "500",
+        bold: "700",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
