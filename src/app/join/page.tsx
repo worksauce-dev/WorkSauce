008e7c9f2e206fe46ea94b7407783370a8eca3aa
ethanJcoding <ethan.95j@gmail.com>
2024-08-28 11:59:38 +0900

    🎨 회원가입 페이지 추가

---
+++ src/app/join/page.tsx
@@ -0,0 +1,11 @@
+import { Join } from "@/components/join/Join";
+import { Header } from "@/components/landing/header/Header";
+
+export default function JoinPage() {
+  return (
+    <>
+      <Header />
+      <Join />
+    </>
+  );
+}
