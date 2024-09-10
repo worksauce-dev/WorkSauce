cd30bd5bd040fd2b8b78b4fc7fbf89e87bf3f5a0
ethanJcoding <ethan.95j@gmail.com>
2024-09-10 17:13:04 +0900

    💄 회원가입 및 로그인 백그라운드 컬러 수정

--- src/components/join/Join.tsx
+++ src/components/join/Join.tsx
@@ -3,9 +3,9 @@ import { JoinForm } from "./JoinForm";
 
 export const Join = () => {
   return (
-    <main className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-16 pt-20 sm:pt-32 flex flex-col lg:flex-row gap-8 justify-center">
+    <main className="min-h-screen bg-gradient-to-b from-blue-50 to-indigo-100 py-12 px-4 sm:px-6 lg:px-16 pt-20 sm:pt-32 flex flex-col lg:flex-row gap-8 justify-center">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl leading-9 font-semibold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl leading-9 font-semibold text-primary-gray ">
           회원가입
         </h2>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -19,11 +19,11 @@ export const Join = () => {
         </div>
         <div className="mx-4 md:mx-0 flex flex-col items-center">
           <div className="my-10 flex w-full items-center">
-            <div className="flex-1 border-t-2 border-gray-200"></div>
+            <div className="flex-1 border-t-2 border-border "></div>
             <span className=" text-sm uppercase mx-5 font-medium text-gray-600">
               또는
             </span>
-            <div className="flex-1 border-t-2 border-gray-200"></div>
+            <div className="flex-1 border-t-2 border-border "></div>
           </div>
           <div className="w-full">
             <button className="w-full shadow rounded-lg bg-white max-w-xs mx-auto flex justify-center py-2 items-center px-8 border hangug border-transparent font-medium  text-gray-700 transition duration-150 ease-in-out hover:opacity-75 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray">
