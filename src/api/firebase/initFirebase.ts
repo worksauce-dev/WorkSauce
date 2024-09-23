f31781a108086c190fa932745ebfbfba788944f3
ethanJcoding <ethan.95j@gmail.com>
2024-09-23 19:01:50 +0900

    ➕ nextauth 추가 및 카카오 oAuth 세팅

--- src/api/firebase/initFirebase.ts
+++ src/api/firebase/initFirebase.ts
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+const firestore = getFirestore(app);
 
-export { auth };
+export { firestore };
