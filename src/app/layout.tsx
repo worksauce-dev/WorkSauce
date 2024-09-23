76eefb7648b0ea98a99b575fb26b088706353be4
ethanJcoding <ethan.95j@gmail.com>
2024-09-23 19:02:27 +0900

    ✨ kakao Oauth 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/landing/header/Header";
 import { Footer } from "@/components/landing/footer/Footer";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+import { firestore } from "@/api/firebase/initFirebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
@@ -21,12 +25,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    const userRef = doc(firestore, "users", session?.user.id);
+    const data = await getDoc(userRef);
+
+    console.log(data.data());
+  }
+
   return (
     <html lang="ko">
       <body
         className={`relative ${pretendard.variable} font-pretendard text-[#1C1C1E] font-normal`}
       >
-        <Header />
+        <Header session={session} />
         {children}
         <Footer />
       </body>
