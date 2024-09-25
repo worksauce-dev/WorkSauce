587a9290fd5f69951facaca15ac6ebde870ec7d8
ethanJcoding <ethan.95j@gmail.com>
2024-09-25 19:30:22 +0900

    💄 home layout 정의 및 기존 파일 구성 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Header } from "@/components/landing/header/Header";
-import { Footer } from "@/components/landing/footer/Footer";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/utils/authOptions";
-import { firestore } from "@/api/firebase/initFirebase";
-import { doc, getDoc } from "firebase/firestore";
 
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
@@ -25,25 +19,12 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession(authOptions);
-
-  if (session) {
-    const userRef = doc(firestore, "users", session?.user.id);
-    const data = await getDoc(userRef);
-
-    if (data.exists()) {
-      console.log(data.data());
-    }
-  }
-
   return (
     <html lang="ko">
       <body
         className={`relative ${pretendard.variable} font-pretendard text-[#1C1C1E] font-normal`}
       >
-        <Header session={session} />
         {children}
-        <Footer />
       </body>
     </html>
   );
