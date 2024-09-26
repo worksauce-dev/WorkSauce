f97df8c459e37d468eeb3eb0bed1664205247f0c
ethanJcoding <ethan.95j@gmail.com>
2024-09-26 13:07:48 +0900

    💄 logo png 변경

--- src/components/landing/header/logo.tsx
+++ src/components/landing/header/logo.tsx
@@ -1,5 +1,6 @@
-import image from "../../../../public/_레이어_1.png";
+import image from "../../../../public/_레이어_2.png";
 import Image from "next/image";
+import textlogo from "../../../../public/textlogo.png";
 
 export const Logo = () => {
   return (
@@ -8,9 +9,7 @@ export const Logo = () => {
       className="flex gap-2 justify-center items-center hover:scale-105 transition"
     >
       <Image src={image} alt="logo" className="w-[24px] sm:w-[48px]" />
-      <span className="text-stroke text-2xl sm:text-4xl font-bold">
-        worksauce
-      </span>
+      <Image src={textlogo} alt="logo" className="w-[100px] sm:w-[200px]" />
     </a>
   );
 };
