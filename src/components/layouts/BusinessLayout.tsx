6eea14b4dfcd8628dd076010a449a1a6c4b4fbcf
ethanJcoding <ethan.95j@gmail.com>
2024-10-07 07:46:43 +0900

    ⚰️ 사용하지 않는 파일 제거

--- src/components/layouts/BusinessLayout.tsx
+++
@@ -1,7 +0,0 @@
-export const BusinessLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="border">{children}</div>;
-};
--- src/components/layouts/PersonalLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from "react";
-
-export const PersonalLayout = ({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
-  return <div className="border">{children}</div>;
-};
