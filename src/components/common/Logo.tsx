53e2906c78fdb58b15c5968e02e5ddd21f174f7b
ethanJcoding <ethan.95j@gmail.com>
2024-11-19 15:38:05 +0900

    💄 로고변경

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,22 +1,17 @@
-import { Droplet } from "lucide-react";
+import Image from "next/image";
+import imagelogo from "../../../public/로고시안1.png";
 
 export const Logo = () => {
   return (
-    // <a
-    //   href="/"
-    //   className="flex gap-2 justify-center items-center hover:scale-105 transition"
-    // >
-    //   <Image src={imagelogo} alt="logo" className="w-[24px] sm:w-[48px]" />
-    //   <Image src={textlogo} alt="logo" className="w-[100px] sm:w-[200px]" />
-    // </a>
     <a
       href="/"
       className="flex justify-center items-center space-x-2 hover:scale-105 transition"
     >
-      <Droplet size={40} className="text-orange-500" />
-      <h1 className="text-4xl font-bold text-gray-900">
-        Work<span className="font-serif italic text-orange-500">Sauce</span>
-      </h1>
+      {/* <Droplet size={40} className="text-orange-500" />
+      <h1 className="text-4xl font-bold text-orange-500">WorkSauce</h1>
+       */}
+
+      <Image src={imagelogo} alt="logo" className="w-[24px] sm:w-[228px]" />
     </a>
   );
 };
