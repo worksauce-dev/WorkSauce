7b929c9e06c396ca3e9d1ccc99726dc087a73d5e
ethanJcoding <ethan.95j@gmail.com>
2024-12-26 03:24:11 +0900

    💄 header의 target 수정

--- src/components/landing/header/Header.tsx
+++ src/components/landing/header/Header.tsx
@@ -5,6 +5,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Logo } from "../../common/Logo";
 import { signOut } from "next-auth/react";
 import { User } from "@/types/user";
+import { MdMenu } from "react-icons/md";
 
 type MenuItem = {
   href?: string;
@@ -54,7 +55,7 @@ export const Header = ({ user }: { user: User | null }) => {
         className="block text-gray-800 hover:text-orange-500 py-2 px-4 rounded-md hover:bg-orange-50 transition-colors"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
-        target={item.href === "login" ? undefined : "_blank"}
+        target={item.label === "도움말" ? "_blank" : undefined}
       >
         {item.label}
       </motion.a>
@@ -72,20 +73,7 @@ export const Header = ({ user }: { user: User | null }) => {
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <MdMenu className="w-6 h-6" />
         </motion.button>
       </nav>
       <AnimatePresence>
