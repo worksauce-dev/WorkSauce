e2a089629f81d5fd87adf8d0ec13c6f8870c0b3f
ethanJcoding <ethan.95j@gmail.com>
2025-01-03 04:32:57 +0900

    ✨ 고스트 세션 핸들 로직 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@ import { Login } from "@/components/login/Login";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 import { redirect } from "next/navigation";
+
 export default async function LoginPage() {
   const session = await getServerSession(authOptions);
 
