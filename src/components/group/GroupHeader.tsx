6b6e6717246596c53ba36c6102079d0a0ef752de
ethanJcoding <ethan.95j@gmail.com>
2025-01-07 20:46:04 +0900

    💄 그룹헤더에 어드민 전용 삭제버튼 추가

--- src/components/group/GroupHeader.tsx
+++ src/components/group/GroupHeader.tsx
@@ -1,38 +1,63 @@
 "use client";
 
-import { Group } from "@/types/group";
 import { getRemainingDays } from "@/utils/groupUtils";
-import { RiDeleteBinLine } from "react-icons/ri";
 import { useRouter } from "next/navigation";
+import { MdDelete } from "react-icons/md";
 
 interface GroupHeaderProps {
-  group: Group;
+  name: string;
   deleteGroup: (groupId: string) => Promise<void>;
+  isDeadline: boolean;
+  deadline: string;
+  groupId: string;
+  isAdmin: boolean;
 }
 
-export default function GroupHeader({ group, deleteGroup }: GroupHeaderProps) {
+export default function GroupHeader({
+  name,
+  deleteGroup,
+  isDeadline,
+  deadline,
+  groupId,
+  isAdmin,
+}: GroupHeaderProps) {
   const router = useRouter();
+
+  const handleDeleteClick = async () => {
+    if (window.confirm("정말로 이 그룹을 삭제하시겠습니까?")) {
+      await deleteGroup(groupId);
+      router.push("/dashboard");
+    }
+  };
+
   return (
-    <div className="flex justify-between items-center mb-6">
-      <h1 className="text-xl font-bold text-gray-900">{group.name} 대시보드</h1>
+    <div className="flex items-center justify-between p-6 ">
+      <div className="flex flex-col">
+        <h1 className="text-xl font-semibold md:text-2xl text-gray-800">
+          {name} 그룹 대시보드
+        </h1>
+      </div>
 
       <div className="flex items-center gap-4">
-        <div className="text-sm text-gray-500">
-          {new Date(group.deadline) > new Date()
-            ? `마감까지 ${getRemainingDays(group.deadline)}일 남음`
-            : "마감 완료"}
-        </div>
-        <button
-          className="p-2 text-red-600 border border-red-600 rounded hover:bg-red-50"
-          onClick={() => {
-            if (confirm("정말로 이 그룹을 삭제하시겠습니까?")) {
-              deleteGroup(group.groupId);
-              router.push("/dashboard");
-            }
-          }}
-        >
-          <RiDeleteBinLine size={16} />
-        </button>
+        {isDeadline && deadline && (
+          <div className="text-sm text-gray-500">
+            {new Date(deadline) > new Date()
+              ? `마감까지 ${getRemainingDays(deadline)}일 남음`
+              : "마감 완료"}
+          </div>
+        )}
+
+        {isAdmin && (
+          <button
+            onClick={handleDeleteClick}
+            className="inline-flex items-center gap-2 px-4 py-2.5 text-sm font-medium text-red-600 bg-red-50 rounded-lg
+          transition-all duration-200 hover:bg-red-100 hover:scale-105 
+          focus:outline-none focus:ring-2 focus:ring-red-200 active:scale-95"
+          >
+            <MdDelete className="w-4 h-4" />
+            그룹 삭제
+          </button>
+        )}
       </div>
     </div>
   );
