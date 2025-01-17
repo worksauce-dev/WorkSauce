0e0611d2d8acd1d240cf66766c4fa9a10293a9ef
ethanJcoding <ethan.95j@gmail.com>
2025-01-17 15:24:07 +0900

    💄 무료로 시작하기 href 변경및 문구 디자인 변경

--- src/components/landing/hero/Hero.tsx
+++ src/components/landing/hero/Hero.tsx
@@ -4,7 +4,11 @@ import { motion } from "framer-motion";
 import Lottie from "react-lottie-player";
 import heroAnimation from "../../../../public/heroPick2.json";
 
-export const Hero = () => {
+interface HeroProps {
+  isLogin: boolean;
+}
+
+export const Hero = ({ isLogin }: HeroProps) => {
   return (
     <main className="overflow-hidden">
       <section className="relative min-h-screen flex items-center">
@@ -17,7 +21,7 @@ export const Hero = () => {
               className="text-center md:text-left"
             >
               <h1 className="flex flex-col gap-2 sm:gap-3 lg:gap-4">
-                <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold whitespace-nowrap">
+                <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold">
                   <span className="bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text text-transparent">
                     인재 선발
                   </span>
@@ -26,20 +30,23 @@ export const Hero = () => {
                     <span className="bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text text-transparent">
                       조직 성장
                     </span>
-                    <span className="absolute -bottom-1 sm:-bottom-2 left-0 w-full h-[2px] sm:h-1 bg-gradient-to-r from-orange-500 to-orange-400 rounded-full"></span>
                   </span>
                   까지
                 </span>
+                <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-extrabold">
+                  <span className="bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text text-transparent">
+                    중소기업{" "}
+                  </span>
+                  <span className="relative">
+                    <span className="bg-gradient-to-r from-orange-500 to-orange-400 bg-clip-text text-transparent">
+                      HR 진단도구 솔루션
+                    </span>
+                  </span>
+                </p>
               </h1>
-              <p className="text-base sm:text-xl md:text-2xl lg:text-3xl mt-4 sm:mt-6 lg:mt-8 mb-8 sm:mb-10 text-gray-600 font-medium tracking-tight">
-                <span className="text-gray-800">중소기업</span>을 위한{" "}
-                <span className="relative inline-block whitespace-nowrap after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-orange-500 after:to-orange-400">
-                  맞춤형 HR 솔루션
-                </span>
-              </p>
               <motion.a
-                href="/login"
-                className="inline-block bg-orange-500 text-white font-bold py-3 px-6 sm:px-8 rounded-full hover:bg-orange-600 transition duration-300"
+                href={isLogin ? "/dashboard" : "/login"}
+                className="mt-4 inline-block bg-orange-500 text-white font-bold py-3 px-6 sm:px-8 rounded-full hover:bg-orange-600 transition duration-300"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
