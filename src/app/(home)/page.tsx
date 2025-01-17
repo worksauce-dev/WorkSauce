b6f8997017d14950159ef677ee0fa22970d563d4
ethanJcoding <ethan.95j@gmail.com>
2025-01-17 15:24:07 +0900

    💄 무료로 시작하기 href 변경및 문구 디자인 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,23 @@
 import { Hero } from "@/components/landing/hero/Hero";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: "워크소스",
   description: "우리 팀에 딱 맞는 지원자 유형",
 };
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+  let isLogin = false;
+
+  if (session) {
+    isLogin = true;
+  }
+
   return (
     <>
-      <Hero />
+      <Hero isLogin={isLogin} />
     </>
   );
 }
