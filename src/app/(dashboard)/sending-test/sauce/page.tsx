18a813f7a2153ee7faf7488e3f4e3fbcc0b04e12
ethanJcoding <ethan.95j@gmail.com>
2025-02-12 20:15:43 +0900

    ✨ 테스트 선택 기능 추가

--- src/app/(dashboard)/sending-test/sauce/page.tsx
+++ src/app/(dashboard)/sending-test/sauce/page.tsx
@@ -5,10 +5,11 @@ import { getUserData } from "@/api/firebase/getUserData";
 import { User } from "@/types/user";
 import { handleAppError } from "@/utils/errorHandler";
 import { ERROR_MESSAGES } from "@/types/error";
-import { TestSelector } from "@/components/TestSelector/TestSelector";
+import { SendingSauceTest } from "@/components/sendingTest/SendingSauceTest";
+import { createGroup } from "@/api/firebase/createGroup";
 
 export const metadata: Metadata = {
-  title: "소스 테스트 - 진단도구 선택",
+  title: "소스 테스트 - 그룹 생성",
 };
 
 export default async function SendingTestPage() {
@@ -25,9 +26,8 @@ export default async function SendingTestPage() {
   }
 
   return (
-    <div className="w-full h-screen flex flex-col lg:flex-row ">
-      {/* <SendingSauceTest user={user} createGroup={createGroup} /> */}
-      <TestSelector user={user} />
+    <div className="w-full h-screen flex flex-col lg:flex-row">
+      <SendingSauceTest user={user} createGroup={createGroup} />
     </div>
   );
 }
