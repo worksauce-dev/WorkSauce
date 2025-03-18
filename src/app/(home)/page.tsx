d7345b88436730357088892ccadf015efe78a5f5
ethanJcoding <ethan.95j@gmail.com>
2025-03-18 15:21:39 +0900

    💄 반응형 디자인 개선 및 모바일 사용자 경험 향상

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/landing/hero/Hero";
+import { HeroMobile } from "@/components/landing/hero/HeroMobile";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 
@@ -16,7 +17,13 @@ export default async function Home() {
 
   return (
     <>
-      <Hero isLogin={isLogin} />
+      <div className="sm:hidden flex flex-col">
+        <HeroMobile isLogin={isLogin} />
+      </div>
+
+      <div className="hidden sm:block">
+        <Hero isLogin={isLogin} />
+      </div>
     </>
   );
 }
