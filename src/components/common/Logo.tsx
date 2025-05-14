fb156ae805cfe04b0a9a2894851d66e858fd87ee
ethanJcoding <ethan.95j@gmail.com>
2025-05-14 20:39:21 +0900

    ✨ 로고 컴포넌트 추가 및 대시보드 관련 컴포넌트 삭제, 코드 정리

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import imagelogo from "../../../public/images/로고시안1.png";
+import imagelogo2 from "../../../public/images/로고시안.png";
+import mobileLogo from "../../../public/images/모바일로고.png";
 
 interface LogoProps {
   className?: string;
@@ -14,6 +16,28 @@ export const Logo = ({ className }: LogoProps) => {
       }`}
     >
       <Image src={imagelogo} alt="logo" className="w-[150px] sm:w-[228px]" />
+    </a>
+  );
+};
+
+export const HeaderLogo = ({ className }: LogoProps) => {
+  return (
+    <a
+      href="/"
+      className={`flex justify-center items-center space-x-2 hover:scale-105 transition ${
+        className || ""
+      }`}
+    >
+      <Image
+        src={imagelogo2}
+        alt="logo"
+        className={`w-[168px] h-8 hidden sm:block ${className}`}
+      />
+      <Image
+        src={mobileLogo}
+        alt="logo"
+        className={`w-6 h-6 block sm:hidden ${className}`}
+      />
     </a>
   );
 };
