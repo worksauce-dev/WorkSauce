68efd0c8a91e131bd374274c3670727c632dfca5
ethanJcoding <ethan.95j@gmail.com>
2025-06-16 21:33:34 +0900

    🔧 코드 정리: EditSauceTest 컴포넌트에서 불필요한 주석 제거 및 기본 구조 수정

--- src/app/(auth)/edit-sauce-test/page.tsx
+++ src/app/(auth)/edit-sauce-test/page.tsx
@@ -1,14 +1,6 @@
-// import TestDBEditor from "@/components/admin/TestDBEditor";
+import { getTestDB } from "@/api/firebase/getTestDB";
+import { SauceTestV2 } from "@/types/saucetestV2Type";
 
-// import { getTestDB } from "@/api/firebase/getTestDB";
-// import { SauceTestV2 } from "@/types/saucetestV2Type";
-
-// export default async function EditSauceTest() {
-//   const testDB = await getTestDB("saucetest");
-
-//   return (
-//     <div className="w-full h-screen flex flex-col lg:flex-row  ">
-//       <TestDBEditor initialData={testDB as SauceTestV2} />
-//     </div>
-//   );
-// }
+export default async function EditSauceTest() {
+  return <div className="w-full h-screen flex flex-col lg:flex-row  "></div>;
+}
